c764753d0ac71fe76faa7a0126c174339e2fffb5
mustafayegnidemir <mustafayegnidemir1@gmail.com>
2026-03-01 16:18:28 +0300

    styling react componenets with css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,9 @@
-import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App() {
     return (
-        // only designed for one card element, but you can easily modify it to render multiple cards if needed 
-
         <>
-        <Card />  
-        <Card /> 
-        <Card />
-        <Card /> 
+            <Button />
     
         </>
     );
